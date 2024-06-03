ac88de4372e496e276ed7c272047755167388177
Diego Ruminot <diegoruminot12@gmail.com>
2024-06-03 00:00:47 -0400

    contraseña

--- Servidor/mysql.js
+++ Servidor/mysql.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'hola123',
     database: 'ferremas_g9'
 });
 
