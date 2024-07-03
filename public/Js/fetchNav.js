09281a4e36a23eff7e3d376baed1a543edaa1580
Diego Ruminot <diegoruminot12@gmail.com>
2024-07-02 22:39:19 -0400

    cambios redirecciones y botones

--- public/Js/fetchNav.js
+++ public/Js/fetchNav.js
@@ -1,49 +1,54 @@
 const login = localStorage.getItem("isLogged");
 const datos = JSON.parse(localStorage.getItem("userData"));
 
-function cerrar(){
+function cerrar() {
     localStorage.setItem("isLogged", "false");
     localStorage.removeItem('userData');
     window.location.href = "/";
 }
 
-if(login=="true"){
+if (login == "true") {
     fetch("/HTML/navWorker.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('navbar-container').innerHTML = data;
-    })
-    .catch(error => console.error('Error cargando el navbar:', error));
-    jQuery(function() {
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-container').innerHTML = data;
+        })
+        .catch(error => console.error('Error cargando el navbar:', error));
+    jQuery(function () {
         $('#datos').empty();
         $('#datos').append(`
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false" id="datos">
-                            Bienvenido, ${datos.nombre} 
+                            aria-expanded="false" id="datos" style="background-color: #28a745; color: inherit; padding: 10px 20px; border-radius: 20px;">
+                            Bienvenido, ${datos.nombre}
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" onclick="cerrar()">Cerrar Sesión</a></li>
+                            <li>
+                                <a class="dropdown-item" onclick="cerrar()" style="background-color: #dc3545; color: inherit; padding: 10px 20px; border-radius: 20px;">
+                                    Cerrar Sesión
+                                </a>
+                            </li>
                         </ul>
+
                     </li>
                 </ul>
             </nav>
         `);
     });
-}else{
+} else {
     fetch("/HTML/navCliente.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('navbar-container').innerHTML = data;
-    })
-    .catch(error => console.error('Error cargando el navbar:', error));
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-container').innerHTML = data;
+        })
+        .catch(error => console.error('Error cargando el navbar:', error));
 }
 fetch("/HTML/footer.html")
-.then(response => response.text())
-.then(data => {
-    document.getElementById('footer-container').innerHTML = data;
-})
-.catch(error => console.error('Error cargando el navbar:', error));
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-container').innerHTML = data;
+    })
+    .catch(error => console.error('Error cargando el navbar:', error));
 
